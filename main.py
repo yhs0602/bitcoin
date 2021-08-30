563982b4be1037cd2b7bb94faca8ca9e5c4e78f7
KYHSGeekCode <jourhyang123@gmail.com>
2021-08-30 21:04:42 +0900

    :sparkles: some api func

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import hashlib
+import os
+import uuid
+from urllib.parse import urlencode
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import jwt  # PyJWT
+import requests
+from dotenv import load_dotenv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def send_without_parameter(path):
+    payload = {
+        'access_key': ACCESS_KEY,
+        'nonce': str(uuid.uuid4()),
+    }
+
+    jwt_token = jwt.encode(payload, SECRET_KEY)
+    authorization_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorization_token}
+    res = requests.get(SERVER_URL + path, headers=headers)
+    return res.json()
 
 
-# Press the green button in the gutter to run the script.
+# key[]=value1&key[]=value2 ...
+def send_with_parameter(path, query):
+    # query는 dict 타입입니다.
+    m = hashlib.sha512()
+    m.update(urlencode(query).encode())
+    query_hash = m.hexdigest()
+
+    payload = {
+        'access_key': ACCESS_KEY,
+        'nonce': str(uuid.uuid4()),
+        'query_hash': query_hash,
+        'query_hash_alg': 'SHA512',
+    }
+
+    jwt_token = jwt.encode(payload, SECRET_KEY)
+    authorization_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorization_token}
+    res = requests.get(SERVER_URL + path, headers=headers)
+    return res
+
+
+def load_envs():
+    global ACCESS_KEY
+    global SECRET_KEY
+    global SERVER_URL
+    load_dotenv()
+    ACCESS_KEY = os.environ.get('ACCESS')
+    SECRET_KEY = os.environ.get('SECRET')
+    SERVER_URL = os.environ.get('URL')
+
+
+def main():
+    load_envs()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
